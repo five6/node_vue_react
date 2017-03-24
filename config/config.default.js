a7b48d6ccfe1dfae0236d8542ddabe393faf18d6
five6 <936333511@qq.com>
2017-03-24 19:38:48 +0800

    add egg dependencies

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1490355041161_6026';
+
+  return config;
+};
