9c4fc3fe73b621cf07655a117c6e5703553854bc
five6 <936333511@qq.com>
2017-06-09 00:55:44 +0800

    a

--- app/schedule/crawler.js
+++ app/schedule/crawler.js
@@ -14,7 +14,6 @@ module.exports = app => {
     };
     config.task = function* (ctx){
         var user_agent_a = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 5_1_1 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3";
-        var user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
         var headers = { 'User-Agent' : user_agent_a };
         var url ="http://news.sina.com.cn/hotnews/";
         var url2 = "http://fulilt.com/thread-19174-1-1.html";
