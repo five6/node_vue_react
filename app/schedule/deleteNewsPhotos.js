ab6e0b0f755d3373af74f96a7bffdc101067e7de
five6 <936333511@qq.com>
2017-06-15 23:59:54 +0800

    添加删除一天前的新闻图片task

--- app/schedule/deleteNewsPhotos.js
+++ app/schedule/deleteNewsPhotos.js
@@ -2,17 +2,47 @@
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const async = require('async');
+const fs = require("fs");
+const path = require('path');
+const BASE_DAYS = 1; //单位 天 >= 0;
 module.exports = app => {
-	var config = {};
-	config.schedule = {
-	 	interval: "1m",
-      	type: "all"
-	}
-	config.task = function* (ctx){
-        let taskObj = {};
-		async.parallel(taskObj,function(err,result){
-			ctx.logger.info("完成一次删除旧信息task");
-		});
-	}
+    var config = {};
+    config.schedule = {
+        interval: "30m",
+        type: "all"
+    }
+    config.task = function*(ctx) {
+        let photoPath = path.join(app.config.baseDir, "app/public/newsImages");
+        let photos = fs.readdirSync(photoPath);
+        let miniSeconds = new Date().valueOf() - 1000 * 60 * 60 * 24 * BASE_DAYS;
+        let filePaths = [];
+        let tasks = [];
+        ctx.logger.info("删除 " + BASE_DAYS + " 天前的新闻图片！");
+        _.each(photos, function (photo) {
+            tasks.push(function (callback) {
+                let path = photoPath + "/" + photo;
+                try{
+                    let state = fs.statSync(path);
+                    let fileTime = new Date(state.ctime).valueOf();
+                    if (fileTime <= miniSeconds) {
+                        fs.unlink(path, function () {
+                            ctx.logger.info("删除图片：" + photo);
+                            callback();
+                        });
+                    }else {
+                        callback();
+                    }
+                }catch (e){
+                    ctx.logger.info(e);
+                    callback();
+                }
+
+            });
+        });
+        async.parallel(tasks, function (err, result) {
+            ctx.logger.info("删除 "+ BASE_DAYS +" 天前的news图片task已经跑了一次 ");
+
+        });
+    }
     return config;
 }
